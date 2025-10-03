ed783e8642b0b4eef618cb72189ca72cdc24a2c9
smileX <120062084+hassen396@users.noreply.github.com>
2025-10-03 19:49:58 +0300

    paract(state): update state of array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Peperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
+  function handleClick(id: number) {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === id ? {...item, quantity: item.quantity++} : item
+      ),
+    });
+  }
   return (
     <div className="flex flex-col w-full min-h-screen items-center mt-5 bg-secondary">
-      <h2>Player info</h2>
-      <p> name: {pizza.name}</p>
-      <p>toppings: </p>
-      <ul>
-        {pizza.toppings.map((topping) => (
-          <li key={topping}>{topping}</li>
-        ))}
-      </ul>
       <button
-        className="bg-blue-500 rounded-2xl px-2 py-1 active:scale-95"
-        onClick={() => {
-          setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-        }}>
-        update your pizza
+        className="bg-blue-500 rounded-xl px-2 py-1 active:scale-95"
+        onClick={() => handleClick(1)}>
+        +
       </button>
     </div>
   );
