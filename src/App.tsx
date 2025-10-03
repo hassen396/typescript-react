3ddfbdb7dd0eefc20d9d008403d2f17d0a854b4e
smileX <120062084+hassen396@users.noreply.github.com>
2025-10-03 15:53:48 +0300

    paract(array): array destructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "abc",
-      age: 12,
-    },
+  const [pizza, setPizza] = useState({
+    name: "Spicy Peperoni",
+    toppings: ["Mushroom"],
   });
   return (
     <div className="flex flex-col w-full min-h-screen items-center mt-5 bg-secondary">
       <h2>Player info</h2>
-      <p> age: {game.player.age}</p>
-      <p>name: {game.player.name}</p>
+      <p> name: {pizza.name}</p>
+      <p>toppings: </p>
+      <ul>
+        {pizza.toppings.map((topping) => (
+          <li key={topping}>{topping}</li>
+        ))}
+      </ul>
       <button
-      className="bg-blue-500 rounded-2xl px-2 py-1 active:scale-95"
+        className="bg-blue-500 rounded-2xl px-2 py-1 active:scale-95"
         onClick={() => {
-          const newgame = {
-            ...game,
-            player: { ...game.player, name: prompt("enter your name")|| game.player.name },
-          };
-          setGame(newgame);
+          setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
         }}>
-        update your name
+        update your pizza
       </button>
     </div>
   );
