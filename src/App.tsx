32ef86c1c65d66e647f6f0b31ed550dbec6c6f9c
smileX <120062084+hassen396@users.noreply.github.com>
2025-10-03 15:26:24 +0300

    pract(nested state): learn how to destructure and update nested states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import Like from "./Components/Like";
+import { useState } from "react";
 
 function App() {
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "abc",
+      age: 12,
+    },
+  });
   return (
     <div className="flex flex-col w-full min-h-screen items-center mt-5 bg-secondary">
-       <Like />
+      <h2>Player info</h2>
+      <p> age: {game.player.age}</p>
+      <p>name: {game.player.name}</p>
+      <button
+      className="bg-blue-500 rounded-2xl px-2 py-1 active:scale-95"
+        onClick={() => {
+          const newgame = {
+            ...game,
+            player: { ...game.player, name: prompt("enter your name")|| game.player.name },
+          };
+          setGame(newgame);
+        }}>
+        update your name
+      </button>
     </div>
   );
 }
