5db60d9e53daff2414e43f495d609ec6eebcc920
smileX <120062084+hassen396@users.noreply.github.com>
2025-10-04 14:42:15 +0300

    feat(ui): update form component (uisng useRef)

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,15 +1,20 @@
-import { useState, type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
 const Form = () => {
-  
-  const [person, setPerson] = useState({
-    name: "",
-    age: '',
-  });
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+  console.log(person)
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log(person);
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null && ageRef.current !== null) {
+      
+      person.name = nameRef.current.value;
+      person.age = parseInt(ageRef.current.value);
+      console.log(person.age, 'age')
+    }
+    console.log(person)
   };
   return (
     <form onSubmit={handleSubmit} onReset={() => console.log("resseted")}>
@@ -17,30 +22,14 @@ const Form = () => {
         <label htmlFor="name" className="">
           Name
         </label>
-        <input
-          value={person.name}
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          id="name"
-          type="text"
-          className=""
-        />
+        <input ref={nameRef} id="name" type="text" className="" />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="">
           Age
         </label>
-        <input
-          value={person.age}
-          onChange={(event) =>
-            setPerson({ ...person, age: event.target.value })
-          }
-          id="age"
-          type="number"
-          className=""
-        />
+        <input ref={ageRef} id="age" type="number" className="" />
       </div>
       <button className="bg-amber-200 px-2 rounded-xl block" type="submit">
         Submit
